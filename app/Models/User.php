dad8cc82752c768eec0d4a4e05006177c3a35b1b
Igor Salgado <salgado.igor.is@gmail.com>
2024-01-17 22:31:36 -0300

    Feat: Verificação de email

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use App\Notifications\RedefinirSenhaNotification;
+use App\Notifications\VerificarEmailNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
@@ -46,5 +47,10 @@ class User extends Authenticatable
     {
         #Passando o objeto de notificação quando for chamado
         $this->notify(new RedefinirSenhaNotification($token, $this->name, $this->email));
+    }
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new VerificarEmailNotification($this->name));
     }
 }
